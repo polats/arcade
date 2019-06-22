09631d9af27f20fc4e04bc58cce696106e0fbbc1
Paul Gadi <paulgadi@gmail.com>
2019-06-22 11:31:16 +0800

    add compiled json, remove broadcaster

---
+++ client/src/contracts/index.js
@@ -0,0 +1,25 @@
+// @create-index
+
+export { default as Address } from './Address.json';
+export { default as AllowanceWrapper } from './AllowanceWrapper.json';
+export { default as Broadcaster } from './Broadcaster.json';
+export { default as CommonConstants } from './CommonConstants.json';
+export { default as ERC1155 } from './ERC1155.json';
+export { default as ERC1155Metadata_URI } from './ERC1155Metadata_URI.json';
+export { default as ERC1155Mintable } from './ERC1155Mintable.json';
+export { default as ERC1155MixedFungible } from './ERC1155MixedFungible.json';
+export { default as ERC1155MixedFungibleMintable } from './ERC1155MixedFungibleMintable.json';
+export { default as ERC1155MockReceiver } from './ERC1155MockReceiver.json';
+export { default as ERC1155ReceiverDelegate } from './ERC1155ReceiverDelegate.json';
+export { default as ERC1155TokenReceiver } from './ERC1155TokenReceiver.json';
+export { default as ERC165 } from './ERC165.json';
+export { default as ERCXXXXReceiverDelegate } from './ERCXXXXReceiverDelegate.json';
+export { default as IERC1155 } from './IERC1155.json';
+export { default as IERC1538 } from './IERC1538.json';
+export { default as Migrations } from './Migrations.json';
+export { default as ProxyBaseStorage } from './ProxyBaseStorage.json';
+export { default as ProxyReceiver } from './ProxyReceiver.json';
+export { default as ProxyReceiverStorage_001_ERC1155MockReceiver } from './ProxyReceiverStorage_001_ERC1155MockReceiver.json';
+export { default as ProxyReceiverStorage_002_ERCXXXXFuture } from './ProxyReceiverStorage_002_ERCXXXXFuture.json';
+export { default as SafeMath } from './SafeMath.json';
+
